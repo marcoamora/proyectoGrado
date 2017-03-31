a0760f9ef28bc385c2fc55b289bfb4dec58116ae
Marcos Mora <marcosmora@unisangil.edu.co>
2017-03-31 11:24:35 -0500

    insercion mapa

--- www/js/map.js
+++ www/js/map.js
@@ -1,7 +1,11 @@
 //inicializacion del mapa
-function initMap(){
+/**function initMap(){
   var map = new google.maps.Map(document.getElementById("map"),{
 	center:{lat:6.55059, lng:-73.136742},
 	zoom: 14
     });
-}
+}*/
+$("#map").geomap({
+  center: [ lat:6.55059, lng:-73.136742],
+  zoom: 14
+});
