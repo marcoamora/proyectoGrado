bdbe3c412fd9c553ee9b332b2a2225920f5d8e56
Marcos Mora <marcosmora@unisangil.edu.co>
2017-03-31 11:38:01 -0500

    creacion de una rama para la construccion de la interfaz

--- www/js/map.js
+++ www/js/map.js
@@ -1,11 +1,7 @@
 //inicializacion del mapa
-/**function initMap(){
+function initMap(){
   var map = new google.maps.Map(document.getElementById("map"),{
 	center:{lat:6.55059, lng:-73.136742},
 	zoom: 14
     });
-}*/
-$("#map").geomap({
-  center: [ lat:6.55059, lng:-73.136742],
-  zoom: 14
-});
+}
